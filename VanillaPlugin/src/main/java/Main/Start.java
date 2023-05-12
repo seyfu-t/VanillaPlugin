f15e1da55a4a5fac72fce0488efb72c0a6b8a97f
seyfu-t <git@seyfu-t.me>
2023-05-13 00:01:53 +0200

    Tab List Time added

--- VanillaPlugin/src/main/java/Main/Start.java
+++ VanillaPlugin/src/main/java/Main/Start.java
@@ -2,10 +2,16 @@ package Main;
 
 import java.io.File;
 import java.io.IOException;
-
+import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Criteria;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 
 import Commands.Point_cmd;
 import Commands.Timber_cmd;
@@ -14,6 +20,7 @@ import Listener.ChatFarbe;
 import Listener.DeathMessage_listener;
 import Listener.SaveAnnounce;
 import Listener.Timber_listener;
+import net.kyori.adventure.text.Component;
 
 public class Start extends JavaPlugin {
 
@@ -28,7 +35,8 @@ public class Start extends JavaPlugin {
 				e.printStackTrace();
 			}
 		}
-
+		schedule();
+		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new ChatFarbe(), this);
 		pm.registerEvents(new DeathMessage_listener(), this);
@@ -43,6 +51,43 @@ public class Start extends JavaPlugin {
 	@Override
 	public void onDisable() {
 
+	}
+	
+	void schedule() {
+	    Scoreboard sc = this.getServer().getScoreboardManager().getMainScoreboard();
+	    Objective obj;
+	    if(sc.getObjective("playtime") == null) {//first time init Scoreboard
+	        obj = sc.registerNewObjective("playtime", Criteria.DUMMY, Component.text("Play Time"));
+	    } else {//init if already exists
+	        obj = sc.getObjective("playtime");
+	    }
+	    
+	    for (Player player : Bukkit.getOnlinePlayers()) {
+	        formatScore(sc, obj, player);
+	    }
+
+	    Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+	        @Override
+	        public void run() {
+	            for (Player player : Bukkit.getOnlinePlayers()) {
+	                formatScore(sc, obj, player);
+	            }
+	        }
+	    }, 0L, 1200L);
+	}
+
+	void formatScore(Scoreboard sc, Objective obj, Player p) {
+	    Score score = obj.getScore(p.getName());
+	    int playtimeTicks = p.getStatistic(Statistic.PLAY_ONE_MINUTE);
+	    int playtimeMinutes = playtimeTicks/(20*60);
+	    int playtimeHours = playtimeMinutes / 60;
+	    int playtimeMinutesRemainder = playtimeMinutes % 60;
+	    String playtimeString = playtimeHours + "h" + playtimeMinutesRemainder + "min";
+	    score.setScore(playtimeMinutes);
+
+	    String displayName = "§7[§b§l" + playtimeString+"§7] §a" + p.getName();
+	    p.playerListName(Component.text(displayName));
+	    p.setScoreboard(sc);
 	}
 
 }
